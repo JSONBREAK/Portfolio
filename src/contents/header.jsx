2b4b1b11e7b95e3bde561db154998bd72699f6f8
JSONBREAK <43241559+JSONBREAK@users.noreply.github.com>
2025-03-10 10:21:21 +0700

    Update header.jsx

--- src/contents/header.jsx
+++ src/contents/header.jsx
@@ -3,10 +3,10 @@ export const data = {
     name: "Jet Surachok",
     title: "Software Developer • Unemployed",
     caption:'A self-taught web developer skilled in HTML, CSS, JavaScript, Dart, and React.js, with a continuous focus on staying up-to-date with the latest technologies.',
-    programing:"Programing: Python (Intermediate), Javascript (Intermediate),SQL (Beginer)",
-    frontend:"Front-End: HTML,CSS, React.js Dart(Futter)",
-    backend:"Back-End: Node.js, Express.js Firebase",
-    tools:"Toos: Git, VS Code, Docker(Basic)",
+    programing:"Programing: Python (Intermediate), Javascript (Intermediate),SQL (Beginer).",
+    frontend:"Front-End: HTML,CSS, React.js Dart(Futter).",
+    backend:"Back-End: Node.js, Express.js Firebase.",
+    tools:"Toos: Git, VS Code, Docker(Basic).",
     btnText: "View Resume",
     link: Resume
 }
