c1e3b22d199296ece4d2ac6717789c4a59a495ec
JSONBREAK <43241559+JSONBREAK@users.noreply.github.com>
2025-03-10 14:02:29 +0700

    newPortfolio

--- src/sections/LeftSection/index.jsx
+++ src/sections/LeftSection/index.jsx
@@ -7,7 +7,7 @@ import NavBar from "../Navbar"
 const LeftSection = () => {
     return(
         <div className="sticky top-14 lg:h-[90vh] grid grid-rows-[70%_30%] gap-6 lg:grid-rows-[3fr_40%_20%] lg:gap-14">
-        <div className='sticky top-14 grid gap-y-5 lg:grid-rows-[40%_30%_30%] lg:h-[87vh]'> 
+        <div className='sticky top-14 grid gap-y-5 lg:grid-rows-[50%_25%_25%] lg:h-[87vh]'> 
           <Header/>
           <NavBar/>
           <Contact/>
--- src/sections/Navbar/index.jsx
+++ src/sections/Navbar/index.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const NavBar = () => {
     return (
-        <div className='flex flex-col gap-3 font-semibold mt-20'>
+        <div className='flex flex-col gap-3 font-semibold mt-6'>
             <div><FontAwesomeIcon className='mr-2 text-PrimaryTitle' icon={faArrowRight}></FontAwesomeIcon> About</div>
             <div><FontAwesomeIcon className='mr-2 text-PrimaryTitle' icon={faArrowRight}></FontAwesomeIcon>Experience</div>
             <div><FontAwesomeIcon className='mr-2 text-PrimaryTitle' icon={faArrowRight}></FontAwesomeIcon>Project</div>
